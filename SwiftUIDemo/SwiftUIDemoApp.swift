40911d8f7fe96ecf51988eee24056f4e84d674f3
AndyFang36 <andylovesapple666@icloud.com>
2025-01-29 20:06:31 +0800

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Andy Fang on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIDemoTests/SwiftUIDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUIDemoTests.swift
+//  SwiftUIDemoTests
+//
+//  Created by Andy Fang on 1/29/25.
+//
+
+import Testing
+
+struct SwiftUIDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
